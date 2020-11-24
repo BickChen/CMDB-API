2582fbdc7bfcf633409edf3c107ad326d914a140
chenxiaoshang <Chen_Yasin@163.com>
2020-11-24 17:22:05 +0800

    报警数据接收API完成

--- app/routers/api/v1/items.py
+++ app/routers/api/v1/items.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter, Request, Form
-from typing import Optional
+from fastapi import APIRouter, Form
 
 
 router = APIRouter()
 
 
 @router.post("/aliyu/")
-def test(*, expression: str = Form(...), metricName: str = Form(...)):
-    print(expression)
-    print(metricName)
+def test(*,
+         alertName: str = Form(...),
+         alertState: str = Form(...),
+         curValue: str = Form(...),
+         dimensions: str = Form(...),
+         instanceName: str = Form(...),
+         metricName: str = Form(...),
+         ):
 
+    data_dict = {
+        "alertname": alertName,
+        "start": alertState,
+        "curvalue": curValue,
+        "instanceid": dimensions,
+        "instancename": instanceName,
+        "metricname": metricName,
+    }
+
+    print(data_dict)
     # return {"status": True}
 
